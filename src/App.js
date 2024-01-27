a9f6b1122d0ad9e9a3bd99158cd74086e52e90dd
Suja Shaj <sujashaj98@gmail.com>
2024-01-27 15:28:49 -0800

    Modified App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
 import { NavigationPanel } from './components/NavigationPanel';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { AddRentals } from './components/AddRentals';
+  import { BrandName } from './components/BrandName';
+// import { RentalProvider } from './context/RentalContext';
 
 function App() {
+
   return (
-    <div className="App">
-      <NavigationPanel/>
-    </div>
+   <div>
+    <BrandName />
+    <NavigationPanel />
+    
+   </div>
+        
+    
   );
 }
 
 export default App;
+
+
