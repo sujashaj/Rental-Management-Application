8d24788f891f9cf2a35275f9fdc192cd0677aa04
Suja Shaj <sujashaj98@gmail.com>
2024-02-08 15:48:06 -0800

    Added sign up page

---
+++ src/components/SignUp.js
@@ -0,0 +1,159 @@
+import * as React from 'react';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import Link from '@mui/material/Link';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+
+const defaultTheme = createTheme({
+    typography: {
+      fontSize: 8,
+    },
+  });
+
+const handleSubmit = async (event) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+    const formData = {
+        username: data.get('firstName'),
+        password: data.get('password'),
+    };
+
+    console.log({
+        username: data.get('firstName'),
+        password: data.get('password'),
+      });
+
+    // You can post the data to the desired endpoint here
+    try {
+        const response = await fetch('http://localhost:5000/login', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData),
+        });
+
+        const responseData = await response.json(); 
+        if (response.ok) {
+            console.log('Login successful!');
+            console.log(responseData);
+        // Redirect or handle successful login here
+        } else {
+        console.error('Login failed.');
+        // Handle login failure
+        }
+    } catch (error) {
+        console.error('Error logging in:', error);
+        // Handle error
+    }
+};
+
+export default function SignUp() {
+
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <Container component="main" 
+      maxWidth="xs"
+      >
+        <CssBaseline />
+        <Box
+          sx={{
+            marginTop: 4,
+            px: 12,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center'
+          }} 
+        >
+          <Avatar sx={{ m: 1, bgcolor: 'secondary.main', width: 24, height: 24 }}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Sign up
+          </Typography>
+          <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 3}}>
+            <Grid container spacing={1}>
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  autoComplete="given-name"
+                  name="firstName"
+                  required
+                  fullWidth
+                  id="firstName"
+                  label="First Name"
+                  autoFocus
+                  size='small'
+                />
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  required
+                  fullWidth
+                  id="lastName"
+                  label="Last Name"
+                  name="lastName"
+                  autoComplete="family-name"
+                  size='small'
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  required
+                  fullWidth
+                  id="email"
+                  label="Email Address"
+                  name="email"
+                  autoComplete="email"
+                  size='small'
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  required
+                  fullWidth
+                  name="password"
+                  label="Password"
+                  type="password"
+                  id="password"
+                  autoComplete="new-password"
+                  size='small'
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <FormControlLabel
+                  control={<Checkbox value="allowExtraEmails" color="primary" />}
+                  label="I want to receive inspiration, marketing promotions and updates via email."
+                />
+              </Grid>
+            </Grid>
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2}}
+            >
+              Sign Up
+            </Button>
+            <Grid container justifyContent="flex-end">
+              <Grid item>
+                <Link href="#" variant="body2">
+                  Already have an account? Sign in
+                </Link>
+              </Grid>
+            </Grid>
+          </Box>
+        </Box>
+      </Container>
+    </ThemeProvider>
+  );
+}
