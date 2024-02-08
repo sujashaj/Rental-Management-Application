f223390cd5a441c146847125cd65d0d763b81815
Suja Shaj <sujashaj98@gmail.com>
2024-02-08 15:51:52 -0800

    Replace react bootstrap with mui

--- src/App.js
+++ src/App.js
@@ -1,22 +1,18 @@
 import './App.css';
 import { NavigationPanel } from './components/NavigationPanel';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { AddRentals } from './components/AddRentals';
-  import { BrandName } from './components/BrandName';
-import { ViewRentals } from './components/ViewRentals';
+
 import { RentalProvider } from './context/RentalContext';
+import SignUp from './components/SignUp';
 // import { RentalProvider } from './context/RentalContext';
 
 function App() {
 
   return (
-    <RentalProvider>
-      <BrandName />
-      <NavigationPanel />
-    </RentalProvider>
     
+    <SignUp />
   );
 }
+
 
 export default App;
 
